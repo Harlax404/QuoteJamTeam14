ffc15c96195e8cb51d5b090a2a4b27a0c73624ca
edwin-od168 <eiodeimi@gmail.com>
2021-09-09 08:24:28 +0200

    Fixed a bug where it would display the multiplier of Player 1 for both Players.

--- QuoteJamTeam14/Assets/Scripts/ScoreManager.cs
+++ QuoteJamTeam14/Assets/Scripts/ScoreManager.cs
@@ -79,7 +79,7 @@ public class ScoreManager : MonoBehaviour
             multiplierTextP1.text = "x" + multiplierP1;
         } else {
             multiplierP2++;
-            multiplierTextP2.text = "x" + multiplierP1;
+            multiplierTextP2.text = "x" + multiplierP2;
         }
     }
 }
